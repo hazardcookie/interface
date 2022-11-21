0c4ac94cf5aea94206a6c6d778890527944edece
Hazard Cookie <wwallace@ripple.com>
2022-11-21 01:23:42 -0500

    Initial commit

---
+++ src/NavBar/MenuItems.js
@@ -0,0 +1,13 @@
+export const MenuItems = [
+  {
+    title: "Swap",
+    url: "/CookieSwap/",
+    cName: "nav-links",
+  },
+  {
+    title: "Liquidity",
+    url: "/CookieSwap/liquidity",
+    cName: "nav-links",
+  },
+  
+];
